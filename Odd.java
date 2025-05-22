2f186e293bd813f397efc7a130829366d57c0477
Garima Mishra <garima26062005@gmail.com>
2025-05-22 09:53:40 +0530

    Create Odd.java

---
+++ Odd.java
@@ -0,0 +1,14 @@
+public class Odd
+{
+    public static void main(String args[])
+    {
+        for(int n=1; n<=100; n++)
+        {
+            if(n%2!=0)
+            {
+                System.out.println(n);
+            }
+        }
+ 
+    }
+}
