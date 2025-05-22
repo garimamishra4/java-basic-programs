43bc3731a4fb8e95903d50e4992cf595af869f66
Garima Mishra <garima26062005@gmail.com>
2025-05-22 09:54:16 +0530

    Create OddSum.java

---
+++ OddSum.java
@@ -0,0 +1,19 @@
+public class OddSum
+{
+    public static void main(String args[])
+    {
+        int a =50;
+        int sum= a *a;
+       /* for(int n=1; n<=100; n++)
+        {
+            if(n%2!=0)
+            {
+                sum+=n;
+            }
+        }*/
+           System.out.println(sum);
+
+ 
+    }
+    
+}
